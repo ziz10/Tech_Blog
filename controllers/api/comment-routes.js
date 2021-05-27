0b87be9ac191f6236a95e83ea4ba7bf94ef52116
Abdoul Bass <malickya8891@gmail.com>
2021-05-26 21:42:37 -0700

    implement post method to create a comment

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -26,23 +26,23 @@ router.get('/:id', (req, res) => {
 });
 
 
-// //route to create a comment
-// router.post('/', withAuth, (req, res) => {
-//     // check session
-//     if (req.session) {
-//     Comment.create({
-//         comment_text: req.body.comment_text, 
-//         post_id: req.body.post_id,
-//         // use the id from the session
-//         user_id: req.session.user_id,
-//     })
-//         .then(dbCommentData => res.json(dbCommentData))
-//         .catch(err => {
-//             console.log(err);
-//             res.status(400).json(err);
-//         })
-//     }
-// });
+//route to create a comment
+router.post('/', withAuth, (req, res) => {
+    // check session
+    if (req.session) {
+    Comment.create({
+        comment_text: req.body.comment_text, 
+        post_id: req.body.post_id,
+        // use the id from the session
+        user_id: req.session.user_id,
+    })
+        .then(dbCommentData => res.json(dbCommentData))
+        .catch(err => {
+            console.log(err);
+            res.status(400).json(err);
+        })
+    }
+});
 
 
 // //route to update a comment
