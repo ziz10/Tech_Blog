19390f6ad203ad47b0de0566186ef06a74c325aa
Abdoul Bass <malickya8891@gmail.com>
2021-05-26 21:53:26 -0700

    implement single post get methold for retrieval

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -37,44 +37,44 @@ router.get('/', (req, res) => {
   
 });
 
-// // GET a single post 
-// router.get('/:id', (req, res) => {
-//     Post.findOne({
-//       where: {
-//         id: req.params.id
-//       },
-//       attributes: ['id', 
-//                    'content',
-//                    'title',
-//                    'created_at'
-//                 ],
-//       include: [
-//         {
-//           model: User,
-//           attributes: ['username']
-//         },
-//         {
-//           model: Comment,
-//           attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
-//           include: {
-//             model: User,
-//             attributes: ['username']
-//           }
-//         }
-//       ]
-//     })
-//       .then(dbPostData => {
-//         if (!dbPostData) {
-//           res.status(404).json({ message: 'No post found with this id' });
-//           return;
-//         }
-//         res.json(dbPostData);
-//       })
-//       .catch(err => {
-//         console.log(err);
-//         res.status(500).json(err);
-//       });
-//   });
+// GET a single post 
+router.get('/:id', (req, res) => {
+    Post.findOne({
+      where: {
+        id: req.params.id
+      },
+      attributes: ['id', 
+                   'content',
+                   'title',
+                   'created_at'
+                ],
+      include: [
+        {
+          model: User,
+          attributes: ['username']
+        },
+        {
+          model: Comment,
+          attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+          include: {
+            model: User,
+            attributes: ['username']
+          }
+        }
+      ]
+    })
+      .then(dbPostData => {
+        if (!dbPostData) {
+          res.status(404).json({ message: 'No post found with this id' });
+          return;
+        }
+        res.json(dbPostData);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
 
 // // creating a post
 // router.post('/', withAuth, (req, res) => {
