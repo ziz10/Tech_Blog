ce77912b53cb8fdcfc2a6c1ba2e370096ef28545
Abdoul Bass <malickya8891@gmail.com>
2021-05-26 22:08:41 -0700

    implement login route

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -60,64 +60,64 @@ router.get('/:id', (req, res) => {
       });
 });
 
-// // POST /api/users - similar to INSERT INTO users / VALUES 
-// router.post('/', (req, res) => {
-//     // expects {username: 'Lernantino', password: 'password1234'}
-//     User.create({
-//         username: req.body.username,
-//         password: req.body.password
-//     })
-//     // store user data during session 
-//     .then(dbUserData => {
-//     req.session.save(() => {
-//         req.session.user_id = dbUserData.id;
-//         req.session.username = dbUserData.username;
-//         req.session.loggedIn = true;
+// POST /api/users - similar to INSERT INTO users / VALUES 
+router.post('/', (req, res) => {
+    // expects {username: 'Lernantino', password: 'password1234'}
+    User.create({
+        username: req.body.username,
+        password: req.body.password
+    })
+    // store user data during session 
+    .then(dbUserData => {
+    req.session.save(() => {
+        req.session.user_id = dbUserData.id;
+        req.session.username = dbUserData.username;
+        req.session.loggedIn = true;
 
-//         res.json(dbUserData);
-//         });
-//     })
-//     .catch(err => {
-//         console.log(err);
-//         res.status(500).json(err);
-//       });
-// });
+        res.json(dbUserData);
+        });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+});
 
 
-// // POST to identify users 
-// router.post('/login', (req, res) => {
-//     // expects {username: 'lernantino', password: 'password1234'}
-//     User.findOne({
-//         where: {
-//             username: req.body.username
-//         }
-//     }).then(dbUserData => {
-//         if (!dbUserData) {
-//             res.status(400).json({ message: 'No user with that username!'});
-//             return;
-//         }
-//         // res.json({ user: dbUserData});
-//         // verify user
-//         const validPassword = dbUserData.checkPassword(req.body.password);
+// POST to identify users 
+router.post('/login', (req, res) => {
+    // expects {username: 'lernantino', password: 'password1234'}
+    User.findOne({
+        where: {
+            username: req.body.username
+        }
+    }).then(dbUserData => {
+        if (!dbUserData) {
+            res.status(400).json({ message: 'No user with that username!'});
+            return;
+        }
+        // res.json({ user: dbUserData});
+        // verify user
+        const validPassword = dbUserData.checkPassword(req.body.password);
 
-//         if (!validPassword) {
-//             res.status(400).json({ message: 'Incorrect password!' });
-//             return;
-//         }
-//         req.session.save(() => {
-//             // declare session variables
-//             req.session.user_id = dbUserData.id;
-//             req.session.username = dbUserData.username;
-//             req.session.loggedIn = true;
+        if (!validPassword) {
+            res.status(400).json({ message: 'Incorrect password!' });
+            return;
+        }
+        req.session.save(() => {
+            // declare session variables
+            req.session.user_id = dbUserData.id;
+            req.session.username = dbUserData.username;
+            req.session.loggedIn = true;
       
-//             res.json({ user: dbUserData, message: 'You are now logged in!' });
-//         });
-//     })
-//     .catch(err => {
-//         console.log(err);
-//         res.status(500).json(err);
-//       });
-// });
+            res.json({ user: dbUserData, message: 'You are now logged in!' });
+        });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+});
 
 
 // // users to log out 
