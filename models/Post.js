73b39ea536c6f0064846ef2c8aa31b7ece119910
Abdoul Bass <malickya8891@gmail.com>
2021-05-27 17:06:38 -0700

    post model created

--- models/Post.js
+++ models/Post.js
@@ -1,36 +1,36 @@
-// module.exports = Post;
-const { Model, DataTypes } = require('sequelize');
-const sequelize = require('../config/connection');
+// // module.exports = Post;
+// const { Model, DataTypes } = require('sequelize');
+// const sequelize = require('../config/connection');
 
-// create our Post model
-class Post extends Model {}
+// // create our Post model
+// class Post extends Model {}
 
-// create fields/columns for Post model
-Post.init(
-    {
-      id: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        primaryKey: true,
-        autoIncrement: true
-      },
-      title: {
-        type: DataTypes.STRING,
-        allowNull: false
-      },
-      content: {
-        type: DataTypes.TEXT,
-        allowNull: false
-      },
-      user_id: {
-        type: DataTypes.INTEGER,
-        references: {
-          model: 'user',
-          key: 'id'
-        }
-      }
-    },
-    {
+// // create fields/columns for Post model
+// Post.init(
+//     {
+//       id: {
+//         type: DataTypes.INTEGER,
+//         allowNull: false,
+//         primaryKey: true,
+//         autoIncrement: true
+//       },
+//       title: {
+//         type: DataTypes.STRING,
+//         allowNull: false
+//       },
+//       content: {
+//         type: DataTypes.TEXT,
+//         allowNull: false
+//       },
+//       user_id: {
+//         type: DataTypes.INTEGER,
+//         references: {
+//           model: 'user',
+//           key: 'id'
+//         }
+//       }
+//     },
+//     {
       sequelize,
       freezeTableName: true,
       underscored: true,
