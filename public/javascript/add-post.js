1c9e41991e9283c7ebaeb11d1114e68a9e25236d
Abdoul Bass <malickya8891@gmail.com>
2021-05-27 17:10:49 -0700

    more logic added on add-post

--- public/javascript/add-post.js
+++ public/javascript/add-post.js
@@ -15,11 +15,11 @@ async function newFormHandler(event) {
       }
     });
   
-//     if (response.ok) {
-//       document.location.replace('/dashboard');
-//     } else {
-//       alert(response.statusText);
-//     }
-//   };
+    if (response.ok) {
+      document.location.replace('/dashboard');
+    } else {
+      alert(response.statusText);
+    }
+  };
   
-// document.querySelector('#new-post-form').addEventListener('submit', newFormHandler);
+document.querySelector('#new-post-form').addEventListener('submit', newFormHandler);
